dc359699095d93336b60ba7c4077c7135246b15c
lukasarnoltt-cmyk <lukasarnoltt@gmail.com>
2026-02-27 21:28:26 +0100

    Update main.js

--- js/main.js
+++ js/main.js
@@ -1,7 +1,7 @@
 window.addEventListener('scroll', () => {
   document.querySelectorAll('section').forEach(section => {
     const top = section.getBoundingClientRect().top;
-    if(top < window.innerHeight - 100) {
+    if (top < window.innerHeight - 100) {
       section.style.opacity = 1;
       section.style.transform = 'translateY(0)';
     }
