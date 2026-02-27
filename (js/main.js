cd057b5009f800725e47e849aea63ad553cd5d7e
lukasarnoltt-cmyk <lukasarnoltt@gmail.com>
2026-02-27 17:11:24 +0100

    Delete (js/main.js

--- (js/main.js
+++
@@ -1,9 +0,0 @@
-window.addEventListener('scroll', () => {
-  document.querySelectorAll('section').forEach(section => {
-    const top = section.getBoundingClientRect().top;
-    if(top < window.innerHeight - 100) {
-      section.style.opacity = 1;
-      section.style.transform = 'translateY(0)';
-    }
-  });
-});
