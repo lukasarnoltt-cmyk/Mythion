da1c578b61f90d943eeb08528fc8fad591ce0bef
lukasarnoltt-cmyk <lukasarnoltt@gmail.com>
2026-03-01 14:17:18 +0100

    Update main_v8.js

--- js/main_v8.js
+++ js/main_v8.js
@@ -70,3 +70,21 @@ flash.classList.add("active");
 setTimeout(() => {
   flash.classList.remove("active");
 }, 300);
+const counters = document.querySelectorAll('.counter');
+
+counters.forEach(counter => {
+  const updateCount = () => {
+    const target = +counter.getAttribute('data-target');
+    const count = +counter.innerText;
+    const increment = target / 100;
+
+    if (count < target) {
+      counter.innerText = Math.ceil(count + increment);
+      setTimeout(updateCount, 20);
+    } else {
+      counter.innerText = target;
+    }
+  };
+
+  updateCount();
+});
